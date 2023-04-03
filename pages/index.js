54e4864f2dbc2ccd8c158a25c1cf697ccea218db
Chandan kumar <chandan.kumar@apnamart.in>
2023-04-03 18:00:15 +0530

    loging page added

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,24 @@
 import Task from "../components/Task"
 import { useEffect } from "react"
+import { useRouter } from 'next/router'
 
 
 export default function Home() {
+  const router=useRouter()
+
+  useEffect(()=>{
+    const userData = JSON.parse(localStorage.getItem('jobUser')??'{}');
+    if(userData?.token){
+        router.push("/tasks")
+    }
+    else{
+      router.push("/login")
+    }
+  },[])
 
   return (
     <>
-    hello
+    ...loading
     </>
   )
 }
